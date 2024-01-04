e652ffc1d918c7925ad4fa1db09da62769fc5fd0
AkshayPatil076 <146464776+AkshayPatil076@users.noreply.github.com>
2024-01-04 12:30:48 +0530

    Update Navb.js
    in this i create a responcive nev-bar

--- src/NAVBAR/Navb.js
+++ src/NAVBAR/Navb.js
@@ -1,4 +1,4 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
 var menu=[
     {
@@ -19,35 +19,58 @@ var menu=[
     }
   
 ];
+
 function Navb(){
+
     return(
         <>
             <nav className="nav-bar">
                 <div className="Left-nav">
                 </div>
                 <div className="Right-nav">
-                    <button id="meenubutt">Meenu</button>
+                    <button id="meenubutt1"  onClick={
+                          
+                        function Show(){
+                            
+                            document.getElementById('ulman').style.display="block";
+                            document.getElementById('meenubutt1').style.display='none';
+                            document.getElementById('meenubutt2').style.display='block';
+                            document.getElementById('m1').style.display='none';
+                          
+                    }}>Show</button>
+                 <button id="meenubutt2"  onClick={
+                          
+                          function Hife(){
+                
+                            document.getElementById('ulman').style.display="none";
+                            document.getElementById('meenubutt2').style.display='none';
+                            document.getElementById('meenubutt1').style.display='block';
+                            document.getElementById('m1').style.display='block';
+                          
+                            
+                      }}>hide</button>
+               
                     <ul id="ulman">
                         {
-                                //Here we call the object by using js //
                                menu.map((menu,index)=>(
                                 <li className='menli' data-index={index}><a href={menu.link} className='menlink'>{menu.menuname}</a></li>
                               ))
                         }
                         
-                        
+
+                        {/* <li className="manli"><a href="" className="menlink">Home</a></li>
+                        <li className="manli"><a href="" className="menlink">Product</a></li>
+                        <li className="manli"><a href="" className="menlink">Contac Us</a></li>
+                        <li className="manli"><a href="" className="menlink">About</a></li>
+                        <li className="manli"><a href="" className="menlink">Services</a></li> */}
                     </ul>
                    
 
-                     <div className="m11">
-                     <button className="m1" onClick={function as(){
-                        alert("Hello Akki")
-                     }}>
+                     <button id="m1" >
                             <b className="bb"> Downlode CV</b>
+                     
                         </button>
-                      
-                     </div>
-                </div>
+                       </div>
                 
             </nav>
         </>
