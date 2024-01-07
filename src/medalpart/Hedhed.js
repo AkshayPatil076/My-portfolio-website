96eed637e9aa043c93cb53615da6784764098afa
AkshayPatil076 <146464776+AkshayPatil076@users.noreply.github.com>
2024-01-07 03:52:59 -0800

    Update Hedhed.js

--- src/medalpart/Hedhed.js
+++ src/medalpart/Hedhed.js
@@ -4,20 +4,17 @@ import Me from './img/IMG20231128184411-removebg-preview.png'
 function Med(){
     return(
         <>
-            <div className='meed'>
-                <div className='left-med'></div>   
-                <div className='right-med'> 
-                        <img src={Me} className='mepic'/>
-                        
-                        {/* <div className='cir2'></div>
-                        <div className='cir3'></div> */}
-                         
-                </div> 
-                <div className='cir1'>
-              
+            <div className='medpart'>
+            <div className='LEFT'>
+                
                 </div>
-            
-             </div>
+                <div className='RIGHT'>
+                    <img src={Me} className='mypicphoto' />
+                </div>
+                <div className='cir1'>
+                    
+                </div>
+            </div>
         </>
     );
 }
